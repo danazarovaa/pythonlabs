bc10fbc509e6fdd669c385ce82414c5c62a3d490
danazarovaa <BataCom1980@gmail.com>
2026-03-05 00:14:47 +0500

    Update 05_zoo.py

--- laba1/05_zoo.py
+++ laba1/05_zoo.py
@@ -2,22 +2,27 @@
 # -*- coding: utf-8 -*-
 
 # есть список животных в зоопарке
-
-zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
+zoo = ['lion', 'kangaroo', 'elephant', 'monkey']
 
 # посадите медведя (bear) между львом и кенгуру
-#  и выведите список на консоль
-# TODO здесь ваш код
+# и выведите список на консоль
+zoo.insert(1, 'bear')
+print(zoo)
 
 # добавьте птиц из списка birds в последние клетки зоопарка
-birds = ['rooster', 'ostrich', 'lark', ]
-#  и выведите список на консоль
-# TODO здесь ваш код
+birds = ['rooster', 'ostrich', 'lark']
+zoo.extend(birds)
+print(zoo)
 
 # уберите слона
-#  и выведите список на консоль
-# TODO здесь ваш код
+# и выведите список на консоль
+zoo.remove('elephant')
+print(zoo)
 
 # выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть понятны простому человеку, не программисту.
-# TODO здесь ваш код
+lion_position = zoo.index('lion') + 1  # +1, чтобы нумерация была с 1, а не с 0
+lark_position = zoo.index('lark') + 1
+
+print(f'Лев сидит в клетке номер {lion_position}')
+print(f'Жаворонок сидит в клетке номер {lark_position}')
